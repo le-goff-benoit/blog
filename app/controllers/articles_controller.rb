6c368055a9045a162b5c586f2051b560f23d9522
Benoit Le Goff <62902815+le-goff-benoit@users.noreply.github.com>
2020-10-05 23:37:15 +0200

    Small blog made with Ruby on Rails

---
+++ app/controllers/articles_controller.rb
@@ -0,0 +1,60 @@
+class ArticlesController < ApplicationController
+  def index
+    @search_term = params[:query]
+    if !@search_term
+      @title = 'Articles'
+      @articles = Article.all
+    else
+      @title = 'Results for: ' + @search_term
+      @articles = Article.where('title LIKE ?', "%#{@search_term}%").or(Article.where('text LIKE ?', "%#{@search_term}%"))
+
+    end
+  end
+
+  def new
+    @title = 'Create a new article'
+  end
+
+  def show
+    @article = Article.find(params[:id])
+    @title = @article.title
+  end
+
+  def results
+    @search_term = params[:query]
+    @title = @search_term
+  end
+
+  def create
+     @article = Article.new(article_params)
+     @article.save
+     redirect_to @article
+  end
+
+  def edit
+    @article = Article.find(params[:id])
+    @title = @article.title
+  end
+
+  def update
+      @article = Article.find(params[:id])
+
+      if @article.update(article_params)
+        redirect_to @article
+      else
+        render 'edit'
+      end
+  end
+
+  def destroy
+    @article = Article.find(params[:id])
+    @article.destroy
+
+    redirect_to articles_path
+  end
+
+  private
+  def article_params
+    params.require(:article).permit(:title, :text)
+  end
+end
---
+++ app/controllers/home_controller.rb
@@ -0,0 +1,9 @@
+class HomeController < ApplicationController
+  def index
+    @last_article = Article.last
+    @last_article.text = @last_article.text[1, 100]
+    @last_article.text += ' ...'
+
+    @articles = Article.all
+  end
+end
